0fa4fc8ce9334208f6ff27bf1fb0f2ded06847d0
roimulia <roimulia1@hotmail.com>
2016-02-14 05:58:08 +0200

    Initial Commit

---
+++ VideoManiplaution/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoManiplaution
+//
+//  Created by roi mulia on 14/02/2016.
+//  Copyright © 2016 com.roimulia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
